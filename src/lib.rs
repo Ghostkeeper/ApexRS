f0d72997f77650cb1beed2f5e1e26d1d57fca12d
Ghostkeeper <rubend@tutanota.com>
2023-04-16 16:55:30 +0200

    Add a coordinate type
    This defines how coordinates are specified throughout the library, a very fundamental data type.

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2023 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+pub mod coordinate;
+pub use coordinate::*;
