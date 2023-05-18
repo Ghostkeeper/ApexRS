950186712349f42f842a5f2c271e1e805503f8fa
Ghostkeeper <rubend@tutanota.com>
2023-05-18 16:03:33 +0200

    Add test for Polygon::new()

---
+++ tests/polygon.rs
@@ -0,0 +1,18 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2023 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+use apex;
+
+/// Test creating a new, empty polygon.
+///
+/// This assert that the new polygon is empty.
+#[test]
+fn new() {
+	let poly = apex::Polygon::new();
+	assert_eq!(poly.len(), 0, "The new polygon has no vertices.");
+}
